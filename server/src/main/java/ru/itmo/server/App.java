1ac366e31d20d6045365af324eed019dc98869a7
Roggired <leshaego14@gmail.com>
2022-04-16 18:19:00 +0300

    Finish examples

--- server/src/main/java/ru/itmo/server/App.java
+++ server/src/main/java/ru/itmo/server/App.java
@@ -1,6 +1,9 @@
 package ru.itmo.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.itmo.common.Request;
+import ru.itmo.common.Response;
 import ru.itmo.common.model.Student;
 
 import java.io.IOException;
@@ -10,6 +13,9 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class App {
+    private static final Logger log = LogManager.getLogger(App.class.getName());
+
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket();
         serverSocket.bind(new InetSocketAddress(65100));
@@ -28,6 +34,24 @@ public class App {
 
         Request request = Request.fromJson(json);
         Student student = request.getArgumentAs(Student.class);
-        System.out.println(student);
+
+        Response response;
+
+        if (request.commandName.equals("add")) {
+            log.info(() -> "Got request add: " + student);
+            response = new Response(
+                    Response.Status.OK,
+                    "Студент успешно добавлен"
+            );
+            socket.getOutputStream().write(response.toJson().getBytes(StandardCharsets.UTF_8));
+        } else {
+            response = new Response(
+                    Response.Status.ERROR,
+                    "Неизвестная команда"
+            );
+        }
+
+        socket.getOutputStream().write(response.toJson().getBytes(StandardCharsets.UTF_8));
+        socket.close();
     }
 }
